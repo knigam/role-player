d733a6af48b788bf7c2bbbed6a9c3445bbdf458d
Kushal Nigam <knigam@users.noreply.github.com>
2024-12-28 20:34:34 -0500

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,2 @@
+export { GameBoard } from "./components/GameBoard";
+export * from "./game/gameEngine";
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts({ include: ["src"] })],
+  build: {
+    lib: {
+      // The entry point for your library
+      entry: path.resolve(__dirname, "src/index.tsx"),
+      // The name of your library (e.g., 'my-library')
+      name: "RolePlayer",
+      // The formats you want to support (CommonJS and ES)
+      fileName: (format) => `role-player.${format}.js`,
+      formats: ["es", "cjs"], // ESM and CommonJS support
+    },
+    rollupOptions: {
+      // Externalize dependencies to avoid bundling them
+      external: ["react", "react-dom"], // Add other dependencies you want to externalize
+      output: {
+        // Ensure the global variable is correct for the UMD format
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    // Minify for production
+    minify: "esbuild",
+    sourcemap: true, // Generate source maps for debugging
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // Optional: Alias for easier imports
+    },
+  },
+});
